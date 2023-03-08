b9e0d703c5460f777d9f751e1e111005adf50c6f
Oriol <urimontilivi@gmail.com>
2023-03-08 19:54:52 +0100

    exercici2 Oriol Carbó

---
+++ Exercici2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercici2
+{
+    public class Exercici2Codi
+    {
+        public string URL = "";
+        public DirectoryInfo directoriInfo;
+        public StringBuilder sb = new StringBuilder();
+
+        public Exercici2Codi(string direct)
+        {
+            directoriInfo = new(direct);
+            URL = direct;
+        }
+
+        public string ObtenirDirectori()
+        {
+            var fitxers = directoriInfo.GetFiles();
+
+            foreach(var fitxer in fitxers)
+            {
+                sb.AppendLine(fitxer.FullName);
+            }
+            return sb.ToString();
+        }
+
+        public List<string> ObtenirDirectoriRecursiu(string ruta)
+        {
+
+            List<string> llistaFitxers = new();
+
+            foreach (var fitxer in Directory.GetFiles(ruta))
+            {
+                llistaFitxers.Add(Path.GetFullPath(fitxer));
+            }
+
+            foreach (var dir in Directory.GetDirectories(ruta))
+            {
+                llistaFitxers.AddRange(ObtenirDirectoriRecursiu(dir));
+            }
+
+            return llistaFitxers;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using Exercici2;
+using System.Net;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ListenAsync();
+        Console.ReadKey();
+    }
+
+    async static void ListenAsync()
+    {
+        
+        HttpListener listener = new HttpListener();
+        listener.Prefixes.Add("http://localhost:51111/");
+        listener.Start();
+
+        while (true)
+        {
+            HttpListenerContext context = await listener.GetContextAsync();
+
+            var url = context.Request.RawUrl;
+
+            string directori = "C:\\Users\\Uri\\Prova";
+            var exercici2 = new Exercici2Codi(directori);
+
+
+            string msg = "";
+            int codiRetorn = (int)HttpStatusCode.OK;
+
+            if (url == null)
+            {
+                msg = "La URL és nula!";
+                codiRetorn = (int)HttpStatusCode.InternalServerError;
+            }
+
+            else
+            {
+                if (url.StartsWith("/llistarDirectori"))
+                {
+                    msg = exercici2.ObtenirDirectori();
+                }
+
+                else if (url.StartsWith("/recursiu"))
+                {
+                    List<String> llista = exercici2.ObtenirDirectoriRecursiu(directori);
+                    foreach(string s in llista)
+                    {
+                        msg += s + "\n";
+                    }
+                }
+
+                context.Response.ContentLength64 = Encoding.UTF8.GetByteCount(msg);
+                context.Response.StatusCode = codiRetorn;
+                using (Stream s = context.Response.OutputStream)
+                using (StreamWriter writer = new StreamWriter(s))
+                    await writer.WriteAsync(msg);
+
+            }
+
+        }
+
+    }
+}
